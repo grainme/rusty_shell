5540f64041c185ae2911a78b426fd6f6053635fc
grainme <marouanebouf3@gmail.com>
2024-12-31 00:40:07 +0100

    stage 4 done

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+
+
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
@@ -9,4 +11,7 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+    let input = input.trim();
+    println!("{input}: command not found");
+
 }
