a6fe54b58e202027560c41d541ee164f69307342
grainme <marouanebouf3@gmail.com>
2024-12-31 01:21:20 +0100

    stage 8 done

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@
 use std::io::{self, Write};
 
 
+fn get_type_response(arg: &str) {
+    if !["echo", "type", "exit"].contains(&arg) {
+        println!("{arg}: not found");
+    } else {
+        println!("{arg} is a shell builtin");
+    }
+}
+
 fn main() {
     // Wait for user input
     loop {
@@ -18,16 +26,17 @@ fn main() {
         
         // dbg!(&input);
         let &cmd = input.first().unwrap();
+        let option = &input[1..].join(" ");
 
         match cmd {
+            "type" => get_type_response(option),
             "exit" => {
-            std::process::exit(input.get(1)
-                .unwrap()
-                .parse()
-                .expect("failed to parse exit code"))
-             },
+                std::process::exit(input.get(1)
+                    .unwrap()
+                    .parse()
+                    .expect("failed to parse exit code"))},
             "echo" => {
-                println!("{}", &input[1..].join(" "));
+                println!("{}", option);
             },
             _ => println!("{}: command not found", cmd),
         }
